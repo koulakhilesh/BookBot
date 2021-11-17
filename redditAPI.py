aa8689e2b3e87957e82f9ed967ecbb61b352ab92
Akhilesh Koul <koulakhilesh@gmail.com>
2021-11-17 17:13:33 +0800

    code updated

---
+++ redditAPI.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+"""
+@author: akhilesh.koul
+
+"""
+
+import requests
+import pandas as pd
+import praw
+from tqdm import tqdm
+import datetime
+import json
+import re
+import time
+
+
+class RedditCorpus():
+    
+    def __init__(self,start_time=None, end_time=None,deltaDay=1,subreddit=None,PostScoreThresh=10,CommentScoreThresh=3,saveLocal=True):
+        self.start_time=start_time
+        self.end_time=end_time
+        self.deltaDay=deltaDay
+        self.subreddit=subreddit
+        self.PostScoreThresh=PostScoreThresh
+        self.CommentScoreThresh=CommentScoreThresh
+        self.saveLocal=saveLocal
+        
+    def getRedditPost(self):
+        self.df_corpus=pd.DataFrame()
+        postTitle=[]
+        postText=[]
+        postScore=[]
+        postID=[]
+        postURL=[]
+
+        self.df_comments=pd.DataFrame()
+        commentID=[]
+        commentScore=[]
+        comment=[]
+        
+        start_time=self.start_time
+        start_time_unix=int(start_time.timestamp())
+        end_time=start_time+datetime.timedelta(days=self.deltaDay)
+        end_time_unix=int(end_time.timestamp())
+
+        range_=(self.end_time - self.start_time)
+        pbar = tqdm(total=int(range_.days/self.deltaDay))
+
+        while end_time <= self.end_time:
+            
+            redditAPI="https://api.pushshift.io/reddit/search/submission/?subreddit="+str(self.subreddit)+"&after="+str(start_time_unix)+"&before="+str(end_time_unix)+"&score=>"+str(self.PostScoreThresh)+"&size=500"
+           
+            try:
+                postjson=requests.get(redditAPI).json()
+                if len(postjson['data'])==0:
+                    pass;
+                if len(postjson['data'])>0:
+                    for i in range(len(postjson['data'])):
+                        postTitle.append(postjson['data'][i]['title'])
+                        try:
+                            postText.append(postjson['data'][i]['selftext'])
+                        except:
+                            postText.append('NA')
+                            
+                        postScore.append(postjson['data'][i]['score'])
+                        postID.append(postjson['data'][i]['id'])
+                        postURL.append(postjson['data'][i]['full_link'])
+                        try:
+                            redditPostCmntListAPI="https://api.pushshift.io/reddit/submission/comment_ids/"+str(postjson['data'][i]['id'])
+                            cmntListjson=requests.get(redditPostCmntListAPI).json()['data']
+                            
+                            cmntList = [str(element) for element in cmntListjson]
+                            cmntListChunks = [cmntList[x:x+100] for x in range(0, len(cmntList), 100)]
+                            
+                            for k in range(len(cmntListChunks)):
+                                
+                                cmntIDListSTR = ",".join(cmntListChunks[k])
+                               
+                                redditPostCmntAPI="https://api.pushshift.io/reddit/comment/search?ids="+str(cmntIDListSTR)+"&sort=desc&sort_type=score"
+                                
+                                try:
+                                    cmntJson=requests.get(redditPostCmntAPI).json()['data']
+                                    cmntJsonFilter = [x for x in cmntJson if x['score'] >= self.CommentScoreThresh]
+                                    if len(cmntJsonFilter)==0:
+                                         pass;
+                                    if len(cmntJsonFilter)>0:   
+                                        for j in range(len(cmntJsonFilter)):
+                                            commentID.append(postjson['data'][i]['id'])
+                                            commentScore.append(cmntJsonFilter[j]['score'])
+                                            comment.append(cmntJsonFilter[j]['body'])
+                                except:
+                                    pass;
+                                    
+                        except:
+                            pass;
+            except:
+                pass;                
+            start_time=end_time
+            start_time_unix=int(start_time.timestamp())
+            end_time=start_time+datetime.timedelta(days=1)
+            end_time_unix=int(end_time.timestamp())
+            pbar.update(1)
+                   
+        pbar.close()
+                
+        self.df_corpus['postTitle']=postTitle
+        self.df_corpus['postText']=postText
+        self.df_corpus['postScore']=postScore
+        self.df_corpus['postID']=postID
+        self.df_corpus['postURL']=postURL
+        
+
+
+        self.df_comments['commentID']=commentID
+        self.df_comments['commentScore']=commentScore
+        self.df_comments['comment']=comment
+        if self.saveLocal==True:  
+            self.df_corpus.to_csv('df_corpus.csv',)
+            self.df_comments.to_csv('df_comments.csv')
+        return self.df_corpus, self.df_comments  
+
+
+    def getBooks(self,df_comments=None):
+        
+        self.df_bookauthor=pd.DataFrame()
+        bookIDList=[]
+        bookAuthorList=[]
+        bookCategoryList=[]
+        bookImageList=[]
+        bookDescriptionList=[]
+        bookPrinttypeList=[]
+        
+        for i in tqdm(range(len(self.df_comments))):
+            stringQuery=self.df_comments['comment'][i]
+            stringQuery=re.sub('&|\*','',stringQuery)
+            stringQueryLine=re.split(r"\s*\n", stringQuery)
+            for j in range(len(stringQueryLine)):
+                try:
+                    googlebooksAPI="https://www.googleapis.com/books/v1/volumes?q="+str(stringQueryLine[j])
+                    booksJson = requests.get(googlebooksAPI)  
+                    try:
+                        bookData = json.loads(booksJson.text)['items']
+                        for k in range(1):
+                           
+                            title=bookData[k]['volumeInfo']['title']
+                            try:
+                                subtitle=bookData[k]['volumeInfo']['subtitle']
+                                title=title +": " + subtitle
+                            except:
+                                pass;
+                            try:
+                                author=bookData[k]['volumeInfo']['authors']
+                            except:
+                                author='NA'        
+                            try:
+                                category=bookData[k]['volumeInfo']['categories']
+                            except:
+                                category='NA'
+                            try:
+                                image=bookData[k]['volumeInfo']['imageLinks']['thumbnail']
+                            except:
+                                image='NA' 
+                            try:
+                                desc=bookData[k]['volumeInfo']['description']
+                            except:
+                                desc='NA'
+                            try:
+                                ptype=bookData[k]['volumeInfo']['printType']
+                            except:
+                                ptype='NA'     
+                       
+                            bookIDList.append(self.df_comments['commentID'][i])
+                            bookAuthorList.append([title,author])  
+                            bookCategoryList.append(category) 
+                            bookImageList.append(image) 
+                            bookDescriptionList.append(desc)
+                            bookPrinttypeList.append(ptype)
+                        
+                          
+                    except:
+                        pattern_all=['\[(.*?)\]','\"(.*?)\"','\*(.*?)\*']
+                        substring=[]
+                        
+                        for l in range(len(pattern_all)):
+                            substring_tmp = re.findall(pattern_all[l], stringQueryLine[j])
+                            substring.extend(substring_tmp)
+                        try:
+                            for m in range(len(substring)):
+                                try:
+                                    googlebooksAPI="https://www.googleapis.com/books/v1/volumes?q="+str(substring[m])
+                                    booksJson = requests.get(googlebooksAPI)   
+                                    try:
+                                        bookData = json.loads(booksJson.text)['items']
+                                    
+                                        for k in range(1):
+                                            title=bookData[k]['volumeInfo']['title']
+                                            try:
+                                                subtitle=bookData[k]['volumeInfo']['subtitle']
+                                                title=title +": " + subtitle
+                                            except:
+                                                pass;
+                                            try:
+                                                author=bookData[k]['volumeInfo']['authors']
+                                            except:
+                                                author='NA'        
+                                            try:
+                                                category=bookData[k]['volumeInfo']['categories']
+                                            except:
+                                                category='NA'
+                                            try:
+                                                image=bookData[k]['volumeInfo']['imageLinks']['thumbnail']
+                                            except:
+                                                image='NA'     
+                                            try:
+                                                desc=bookData[k]['volumeInfo']['description']
+                                            except:
+                                                desc='NA'
+                                            try:
+                                                ptype=bookData[k]['volumeInfo']['printType']
+                                            except:
+                                                ptype='NA'     
+                                         
+                                            bookIDList.append(self.df_comments['commentID'][i])
+                                            bookAuthorList.append([title,author])  
+                                            bookCategoryList.append(category) 
+                                            bookImageList.append(image) 
+                                            bookDescriptionList.append(desc)
+                                            bookPrinttypeList.append(ptype)
+                            
+                                       
+                                            
+                                    except:
+                                        pass;
+                                except:
+                                    pass;
+                                                
+                        except:
+                            pass;                         
+                  
+                except:  
+                    pass;
+                                  
+      
+        self.df_bookauthor['bookID']=bookIDList
+        self.df_bookauthor['bookAuthor']=bookAuthorList
+        self.df_bookauthor['bookCategory']=bookCategoryList
+        self.df_bookauthor['bookImage']=bookImageList
+        self.df_bookauthor['bookDescription']=bookDescriptionList                                    
+        self.df_bookauthor['bookPrinttype']=bookPrinttypeList                                    
+        self.df_bookauthor['book']= self.df_bookauthor.bookAuthor.apply(lambda x: x[0])
+        self.df_bookauthor=self.df_bookauthor.copy()
+        self.df_bookauthor.drop_duplicates(subset=['bookID', 'book'], keep='first',inplace=True)
+        if self.saveLocal==True:  
+            self.df_bookauthor.to_csv('df_bookauthor.csv')
+            
+        return self.df_bookauthor
+             
+
+if __name__ == '__main__':
+    
+    
+    RedditCorpusClass=RedditCorpus(start_time=datetime.datetime(2018, 1, 1),
+                                   end_time=datetime.datetime(2021, 11, 15),
+                                   deltaDay=1,
+                                   subreddit='suggestmeabook',
+                                   PostScoreThresh=10,
+                                   CommentScoreThresh=3,
+                                   saveLocal=True)
+    
+                                 
+    df_corpus,df_comments=RedditCorpusClass.getRedditPost()
+    df_bookauthor=RedditCorpusClass.getBooks()                
+    
+    
---
+++ suggestmodel.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: akhilesh.koul
+
+"""
+
+
+import requests
+import pandas as pd
+import praw
+from tqdm import tqdm
+import datetime
+import json
+import re
+import time
+import numpy as np
+import nltk
+from io import BytesIO
+import matplotlib.pyplot as plt
+from PIL import Image
+import pickle
+from nltk.corpus import stopwords
+from collections import Counter
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from sklearn import preprocessing
+from ast import literal_eval
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.neighbors import NearestNeighbors
+
+
+
+class suggestModel():
+    
+    def __init__(self,df_corpus_path=None,df_bookauthor_path=None,df_merge_path=None):
+        
+        self.df_corpus_path=df_corpus_path
+        self.df_text=pd.read_csv(df_corpus_path)
+        self.df_text["postTitle"].replace(np.nan, "",inplace=True)
+        self.df_text["text"] = self.df_text["postTitle"].astype(str) + str(" ") + self.df_text["postText"].astype(str)
+        
+        self.df_bookauthor_path=df_bookauthor_path
+        self.df_book=pd.read_csv(df_bookauthor_path)
+        self.df_book=self.df_book[self.df_book['bookPrinttype']=='BOOK']
+   
+        self.df_merge_path=df_merge_path
+       
+     
+        
+    def cleanText(self):
+       
+        self.df_text["text"] = self.df_text["text"].str.lower()
+        #remove_punctuation
+        self.df_text["text"] = self.df_text["text"].apply(lambda text: self.remove_punctuation(text,train=True))
+        #remove_stopwords
+        self.df_text["text"] = self.df_text["text"].apply(lambda text: self.remove_stopwords(text,train=True)) 
+        # remove_freqwords
+        self.df_text["text"] = self.df_text["text"].apply(lambda text: self.remove_freqwords(text,train=True))
+        #lemmatize_words
+        self.df_text["text"] = self.df_text["text"].apply(lambda text: self.lemmatize_words(text))
+        return self.df_text
+
+
+    def remove_punctuation(self,text,train=False):
+        
+        if train==True:
+            PUNCT_TO_REMOVE = '!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}~”“`'
+            with open('PUNCT_TO_REMOVE.pkl', 'wb') as f:
+                   pickle.dump(PUNCT_TO_REMOVE, f)
+       
+        with open('PUNCT_TO_REMOVE.pkl', 'rb') as f:
+            PUNCT_TO_REMOVE = pickle.load(f)      
+        return text.translate(str.maketrans('', '', PUNCT_TO_REMOVE))
+
+
+    def remove_stopwords(self,text,train=False):
+        STOPWORDS = set(stopwords.words('english'))
+      
+        return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+    
+    
+    def remove_freqwords(self, text,train=False):
+        if train==True:
+            cnt = Counter()
+            for text in self.df_text["text"].values:
+                for word in text.split():
+                    cnt[word] += 1
+            with open('mostCommonWords.pkl', 'wb') as f:
+                        pickle.dump(cnt.most_common(10), f)
+            mostCommonWords= cnt.most_common(10)
+        
+        with open('mostCommonWords.pkl', 'rb') as f:
+                mostCommonWords = pickle.load(f) 
+        FREQWORDS = set([w for (w, wc) in mostCommonWords])
+        return " ".join([word for word in str(text).split() if word not in FREQWORDS])
+
+    def lemmatize_words(self,text):
+        lemmatizer = WordNetLemmatizer()
+        wordnet_map = {"N":wordnet.NOUN, "V":wordnet.VERB, "J":wordnet.ADJ, "R":wordnet.ADV}
+        pos_tagged_text = nltk.pos_tag(text.split())
+        return " ".join([lemmatizer.lemmatize(word, wordnet_map.get(pos[0], wordnet.NOUN)) for word, pos in pos_tagged_text])
+
+
+    def trainData(self):
+        self.df_text['bookID']=self.df_text['postID']
+        
+        self.df = pd.merge(self.df_book, self.df_text, how="left", on=["bookID"])
+        self.df=self.df[self.df['bookPrinttype']=='BOOK']
+        self.df.to_csv('df_merge.csv')
+        
+    
+        tfidfvectorizer = TfidfVectorizer()
+        df_text_vector = tfidfvectorizer.fit_transform(self.df['text'])
+        with open('tfidfvectorizer.pkl', 'wb') as f:
+               pickle.dump(tfidfvectorizer, f)
+        
+        tfidftransformer = TfidfTransformer()
+        df_text_tfidf = tfidftransformer.fit_transform(df_text_vector)
+     
+        with open('tfidftransformer.pkl', 'wb') as f:
+               pickle.dump(tfidftransformer, f)
+        
+        neighModel = NearestNeighbors(n_neighbors=5)
+        neighModel.fit(df_text_tfidf)
+        
+        with open('neighModel.pkl', 'wb') as f:
+               pickle.dump(neighModel, f)
+        
+
+    def suggestmeabook(self,text):
+        
+        text=self.remove_punctuation(text)
+        text=self.remove_stopwords(text)
+        text=self.remove_freqwords(text)
+        text=self.lemmatize_words(text)
+        text_list=[]
+        text_list.append(text)
+        df_single=pd.DataFrame()
+        df_single['text']=text_list
+    
+        with open('tfidfvectorizer.pkl', 'rb') as f:
+            tfidfvectorizer = pickle.load(f)  
+               
+        with open('tfidftransformer.pkl', 'rb') as f:
+            tfidftransformer = pickle.load(f)  
+            
+        df_text_vector = tfidfvectorizer.transform(df_single['text'])
+        df_text_tfidf = tfidftransformer.transform(df_text_vector)
+        
+        with open('neighModel.pkl', 'rb') as f:
+            neighModel = pickle.load(f)  
+        
+        y_pred = neighModel.kneighbors(df_text_tfidf)
+        
+        df_merge=pd.read_csv(self.df_merge_path)
+        df_book=pd.read_csv(self.df_bookauthor_path)
+       
+        for i in range(len(y_pred[1][0])):
+            if i == 0:
+                sample_dfs=df_book[df_book['bookID']==df_merge.iloc[y_pred[1][0][i]]['bookID']]
+            else:
+                sample_df=df_book[df_book['bookID']==df_merge.iloc[y_pred[1][0][i]]['bookID']]
+                sample_dfs=sample_dfs.append(sample_df)
+        
+        first_pass=sample_dfs.drop_duplicates(subset='book', keep="first")   
+        second_pass=first_pass.loc[first_pass.book.str.extract(r'(.*):').drop_duplicates().index]
+        second_pass.dropna(subset=['bookImage'],inplace=True)
+        
+        sampleFive=sample_df.sample(n=5)
+        sampleFive.reset_index(drop=True,inplace=True)
+        
+        for i in range(len(sampleFive)):
+            print(sampleFive.iloc[i]['book'])
+            response = requests.get(sampleFive.iloc[i]['bookImage'])
+            img = Image.open(BytesIO(response.content))
+            imgplt=plt.imshow(img)
+            plt.show()
+            
+            
+        # return sampleFive    
+        
+
+
+
+if __name__ == '__main__':
+    
+    suggestModelClass=suggestModel(df_corpus_path='df_corpus.csv',df_bookauthor_path='df_bookauthor.csv',df_merge_path='df_merge.csv')
+    df_clean=suggestModelClass.cleanText()
+    suggestModelClass.trainData()
+    text= 'books like fiction, like the alchemist and the hundred years of solitude'
+    suggestModelClass.suggestmeabook(text)
+    
